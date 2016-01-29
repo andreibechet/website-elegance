de627bc7ef6e4713e491f38a231667a979f26161
unknown <ionudanic@bitbucket.org>
2016-01-29 16:38:19 +0100

    temp commit gallery-detail

--- catrinel_menghia/www/app/acasa/acasa.controller.js
+++ catrinel_menghia/www/app/acasa/acasa.controller.js
@@ -2,34 +2,16 @@
  * Acasa controllers
  */
 (function () {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('starter.controllers')
-        .controller('AcasaCtrl', AcasaCtrl);
+  angular
+    .module('starter.controllers')
+    .controller('AcasaCtrl', AcasaCtrl);
 
-    function AcasaCtrl($scope, getHomeCarouselGallery) {
-        getHomeCarouselGallery.async().then(function() {
-            $scope.data = getHomeCarouselGallery.data();
-        });
-
-        console.log('Acasa controller');
-
-        // $scope.data = [
-        //     {
-        //         url: "img/carousel/catalog_elegance_20162.jpg",
-        //         text: "Noua colectie 2016?"
-        //     },
-        //     {
-        //         url: "img/carousel/catalog_elegance_20163.jpg",
-        //         text: "Hai la noi la magazin"
-        //     },
-        //     {
-        //         url: "img/carousel/catalog_elegance_20165.jpg",
-        //         text: "Vindem gogosi"
-        //     }
-        // ]
-
-    }
+  function AcasaCtrl($scope, getHomeCarouselGallery) {
+    getHomeCarouselGallery.async().then(function () {
+      $scope.data = getHomeCarouselGallery.data();
+    });
+  }
 
 })();
--- catrinel_menghia/www/app/gallery/galleries.controller.js
+++ catrinel_menghia/www/app/gallery/galleries.controller.js
@@ -2,24 +2,45 @@
  * Galleries controllers
  */
 (function () {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('starter.controllers')
-        .controller('GalleriesCtrl', GalleriesCtrl)
-        .controller('GalleryDetailCtrl', GalleryDetailCtrl);
+  angular
+    .module('starter.controllers')
+    .controller('GalleriesCtrl', GalleriesCtrl)
+    .controller('GalleryDetailCtrl', GalleryDetailCtrl);
 
-    function GalleriesCtrl($scope, getGalleries) {
-        getGalleries.async().then(function () {
-            $scope.data = getGalleries.data();
-            console.log($scope.data);
-        });
-    }
+  function GalleriesCtrl($scope, getGalleries) {
+    getGalleries.async().then(function () {
+      $scope.data = getGalleries.data();
+    });
+  }
 
-    function GalleryDetailCtrl($scope, getGalleriesByNid) {
-        getGalleriesByNid.async().then(function () {
-            $scope.data = getGalleriesByNid.data();
-        });
-    }
+  function GalleryDetailCtrl($scope, getGalleriesByNid, $ionicModal) {
+    getGalleriesByNid.async().then(function () {
+      $scope.data = getGalleriesByNid.data();
+    });
+
+    //modal functions
+    $ionicModal.fromTemplateUrl('gallery-modal.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function (modal) {
+      $scope.modal = modal;
+    });
+
+    $scope.openModal = function () {
+      $scope.modal.show();
+    };
+
+    $scope.closeModal = function () {
+      $scope.modal.hide();
+    };
+
+    //Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function () {
+      $scope.modal.remove();
+    });
+
+  }
 
 })();
