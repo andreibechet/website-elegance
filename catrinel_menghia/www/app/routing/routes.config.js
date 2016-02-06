2a9e8048b4a33956411c827971dca6592291e92c
Daniel Ionut Codila <daniel.codila@TIEKinetix.com>
2016-02-06 22:25:20 +0100

    Scrolling fix

--- catrinel_menghia/www/app/routing/routes.config.js
+++ catrinel_menghia/www/app/routing/routes.config.js
@@ -11,84 +11,54 @@
     function routes($stateProvider, $urlRouterProvider) {
         $stateProvider
 
-            .state('app', {
-                url: '/app',
-                abstract: true,
-                templateUrl: 'layout/menu.html'
-            })
-
-            .state('app.acasa', {
+            .state('acasa', {
                 url: '/acasa',
-                views: {
-                    'menuContent': {
-                        templateUrl: 'app/acasa/acasa.html',
-                        controller: 'AcasaCtrl'
-                    }
-                }
+                templateUrl: 'app/acasa/acasa.html',
+                controller: 'AcasaCtrl'
+
             })
 
-            .state('app.magazine', {
+            .state('magazine', {
                 url: '/magazine',
-                views: {
-                    'menuContent': {
-                        templateUrl: 'app/magazine/magazine.html',
-                        controller: 'MagazineCtrl'
-                    }
-                }
+                templateUrl: 'app/magazine/magazine.html',
+                controller: 'MagazineCtrl'
+
             })
 
-            .state('app.colaboratori', {
+            .state('colaboratori', {
                 url: '/colaboratori',
-                views: {
-                    'menuContent': {
-                        templateUrl: 'app/colaboratori/colaboratori.html',
-                        controller: 'ColaboratoriCtrl'
-                    }
-                }
+                templateUrl: 'app/colaboratori/colaboratori.html',
+                controller: 'ColaboratoriCtrl'
             })
 
-            .state('app.targ', {
+            .state('targ', {
                 url: '/targ',
-                views: {
-                    'menuContent': {
-                        templateUrl: 'app/targ/targ.html',
-                        controller: 'TargCtrl'
-                    }
-                }
+                templateUrl: 'app/targ/targ.html',
+                controller: 'TargCtrl'
+
             })
 
-            .state('app.contact', {
+            .state('contact', {
                 url: '/contact',
-                views: {
-                    'menuContent': {
-                        templateUrl: 'app/contact/contact.html',
-                        controller: 'ContactCtrl'
-                    }
-                }
+                templateUrl: 'app/contact/contact.html',
+                controller: 'ContactCtrl'
+
             })
 
-            .state('app.galleries', {
+            .state('galleries', {
                 url: '/galleries',
-                views: {
-                    'menuContent': {
-                        templateUrl: 'app/gallery/galleries.html',
-                        controller: 'GalleriesCtrl'
-                    }
-                }
+                templateUrl: 'app/gallery/galleries.html',
+                controller: 'GalleriesCtrl'
             })
 
-            .state('app.single', {
+            .state('single', {
                 url: '/galleries/:galleryId',
-                views: {
-                    'menuContent': {
-                        templateUrl: 'app/gallery/gallery.html',
-                        controller: 'GalleryDetailCtrl'
-                    }
-                }
+                templateUrl: 'app/gallery/gallery.html',
+                controller: 'GalleryDetailCtrl'
             });
 
         // if none of the above states are matched, use this as the fallback
-        $urlRouterProvider.otherwise('/app/galleries');
+        $urlRouterProvider.otherwise('/acasa');
 
     }
 })();
